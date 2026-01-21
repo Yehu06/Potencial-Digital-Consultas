7007238d3040444f814d402b8ca428c090e2a2cf
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2026-01-21 19:44:27 +0100

    Update SQLDefault.sql

--- SQLDefault.sql
+++ SQLDefault.sql
@@ -1,7 +1,13 @@
+
 ALTER TABLE ponentes 
 ADD CONSTRAINT DF_ponentes_sueldo 
 DEFAULT 0 FOR sueldo;
+--He usado un alter table para no tener que borrar y crear otra vez la tabla de nuevo. He puesto que si no se pone un sueldo que se ponga como 0 por defecto
+
+
+
 
 INSERT INTO ponentes (nombre)
 VALUES
-('María López');
+
+('MarÃ­a LÃ³pez');
